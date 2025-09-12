913ce7692bc23037fe5bd4fd61293bc5b7defaf6
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-09-12 10:29:31 +0200

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,11 @@
-import Product from "./Product";
+import Alert from "./Alert";
 
 export default function App() {
   return (
     <>
-      <h1>Best selling</h1>
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
-        price={14.29}
-      />
+      <Alert />
+      <Alert type="success" />
+      <Alert type="error" />
     </>
   );
 }
