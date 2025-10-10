54188065e9d72c6f9deeb17214a14951ee05722e
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-10-10 11:15:28 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,23 +14,31 @@ import { useState } from "react";
 
 export default function App() {
   const [articles, setArticles] = useState<Article[]>([]);
+  // 1. Додаємо стан індикатора завантаження
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async (topic: string) => {
+    // 2. змінюємо індикатор на true перед запитом
+    setIsLoading(true);
     const response = await axios.get<ArticlesHttpResponse>(
       `https://hn.algolia.com/api/v1/search?query=${topic}`
     );
+    // 3. Змінюємо індикатор на false після запиту
+    setIsLoading(false);
     setArticles(response.data.hits);
   };
 
   return (
     <>
       <SearchForm onSubmit={handleSearch} />
+      {/* 4. Відображаєм повідомлення про завантаження даних в JSX */}
+      {isLoading && <p>Loading data, please wait...</p>}
       {articles.length > 0 && (
         <ul>
-          {articles.map(({ objectID, url, title }) => (
-            <li key={objectID}>
-              <a href={url} target="_blank">
-                {title}
+          {articles.map((article) => (
+            <li key={article.objectID}>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                {article.title}
               </a>
             </li>
           ))}
