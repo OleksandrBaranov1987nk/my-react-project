0f9b966f1bf532e07f33f4040ab19029939d968e
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-10-10 11:04:49 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,19 +10,32 @@ interface Article {
 interface ArticlesHttpResponse {
   hits: Article[];
 }
+import { useState } from "react";
 
 export default function App() {
+  const [articles, setArticles] = useState<Article[]>([]);
+
   const handleSearch = async (topic: string) => {
-    // Виконуємо HTTP-запит
     const response = await axios.get<ArticlesHttpResponse>(
       `https://hn.algolia.com/api/v1/search?query=${topic}`
     );
-    console.log(response.data); // об'єкт з властивістю hits
+    setArticles(response.data.hits);
   };
 
   return (
     <>
       <SearchForm onSubmit={handleSearch} />
+      {articles.length > 0 && (
+        <ul>
+          {articles.map(({ objectID, url, title }) => (
+            <li key={objectID}>
+              <a href={url} target="_blank">
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
