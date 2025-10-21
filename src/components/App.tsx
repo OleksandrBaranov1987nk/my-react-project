9307a851b99561d5acf98c9623b131934e5703c1
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-10-21 10:36:56 +0200

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
-import axios from "axios";
 import SearchForm from "./SearchForm";
 import { useState } from "react";
 import ArticleList from "./ArticleList";
+import { fetchArticles } from "../services/articleService";
 
 interface Article {
   objectID: string;
@@ -10,32 +10,21 @@ interface Article {
   author: string;
 }
 
-interface ArticlesHttpResponse {
-  hits: Article[];
-}
-
 export default function App() {
   const [articles, setArticles] = useState<Article[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  // 1. Оголошуємо стан
   const [isError, setIsError] = useState(false);
 
   const handleSearch = async (topic: string) => {
-    // 2. Додаємо блок try...catch
     try {
       setIsLoading(true);
-      // 3. Скидаємо стан помилки в false перед кожним запитом
       setIsError(false);
-      const response = await axios.get<ArticlesHttpResponse>(
-        `https://hn.algolia.com/api/v1/search?query=${topic}`
-      );
-      setArticles(response.data.hits);
+      // 2. Використовуємо HTTP-функцію
+      const data = await fetchArticles(topic);
+      setArticles(data);
     } catch {
-      // 4. Встановлюємо стан isError в true
       setIsError(true);
     } finally {
-      // 5. Встановлюємо стан isLoading в false
-      // після будь якого результату запиту
       setIsLoading(false);
     }
   };
@@ -44,7 +33,6 @@ export default function App() {
     <>
       <SearchForm onSubmit={handleSearch} />
       {isLoading && <p>Loading data, please wait...</p>}
-      {/* 6. Використовуємо стан isError щоб показати помилку */}
       {isError && <p>Whoops, something went wrong! Please try again!</p>}
       {articles.length > 0 && <ArticleList items={articles} />}
     </>
