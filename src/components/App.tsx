83c830997f340b018488fb2c74559a8b60a38574
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-10-21 10:28:39 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,25 +16,36 @@ interface ArticlesHttpResponse {
 
 export default function App() {
   const [articles, setArticles] = useState<Article[]>([]);
-  // 1. Додаємо стан індикатора завантаження
   const [isLoading, setIsLoading] = useState(false);
+  // 1. Оголошуємо стан
+  const [isError, setIsError] = useState(false);
 
   const handleSearch = async (topic: string) => {
-    // 2. змінюємо індикатор на true перед запитом
-    setIsLoading(true);
-    const response = await axios.get<ArticlesHttpResponse>(
-      `https://hn.algolia.com/api/v1/search?query=${topic}`
-    );
-    // 3. Змінюємо індикатор на false після запиту
-    setIsLoading(false);
-    setArticles(response.data.hits);
+    // 2. Додаємо блок try...catch
+    try {
+      setIsLoading(true);
+      // 3. Скидаємо стан помилки в false перед кожним запитом
+      setIsError(false);
+      const response = await axios.get<ArticlesHttpResponse>(
+        `https://hn.algolia.com/api/v1/search?query=${topic}`
+      );
+      setArticles(response.data.hits);
+    } catch {
+      // 4. Встановлюємо стан isError в true
+      setIsError(true);
+    } finally {
+      // 5. Встановлюємо стан isLoading в false
+      // після будь якого результату запиту
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
       <SearchForm onSubmit={handleSearch} />
-      {/* 4. Відображаєм повідомлення про завантаження даних в JSX */}
       {isLoading && <p>Loading data, please wait...</p>}
+      {/* 6. Використовуємо стан isError щоб показати помилку */}
+      {isError && <p>Whoops, something went wrong! Please try again!</p>}
       {articles.length > 0 && <ArticleList items={articles} />}
     </>
   );
