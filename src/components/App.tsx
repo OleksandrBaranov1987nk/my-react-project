5057d38b8999146c2a05e20ad1b13878eee55af1
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-10-21 10:05:48 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,44 +1,39 @@
 import axios from "axios";
 import SearchForm from "./SearchForm";
+import { useState } from "react";
 
 interface Article {
   objectID: string;
   title: string;
   url: string;
+  author: string;
 }
 
 interface ArticlesHttpResponse {
   hits: Article[];
 }
-import { useState } from "react";
 
 export default function App() {
   const [articles, setArticles] = useState<Article[]>([]);
-  // 1. Додаємо стан індикатора завантаження
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async (topic: string) => {
-    // 2. змінюємо індикатор на true перед запитом
-    setIsLoading(true);
     const response = await axios.get<ArticlesHttpResponse>(
       `https://hn.algolia.com/api/v1/search?query=${topic}`
     );
-    // 3. Змінюємо індикатор на false після запиту
-    setIsLoading(false);
     setArticles(response.data.hits);
   };
+  console.log(articles);
 
   return (
     <>
       <SearchForm onSubmit={handleSearch} />
-      {/* 4. Відображаєм повідомлення про завантаження даних в JSX */}
-      {isLoading && <p>Loading data, please wait...</p>}
       {articles.length > 0 && (
         <ul>
-          {articles.map((article) => (
-            <li key={article.objectID}>
-              <a href={article.url} target="_blank" rel="noopener noreferrer">
-                {article.title}
+          {articles.map(({ objectID, url, title, author }) => (
+            <li key={objectID}>
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                {title}
+                {author ? ` — ${author}` : ""}
               </a>
             </li>
           ))}
