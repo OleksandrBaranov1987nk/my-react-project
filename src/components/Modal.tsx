674dd14bbe743637683e3a8356ea4b719e123fee
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-10-23 10:27:58 +0200

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,8 +1,14 @@
+import { createPortal } from "react-dom";
+import css from "./Modal.module.css";
+
 export default function Modal() {
-  return (
-    <div>
-      <h2>Modal Title</h2>
-      <p>This is some content inside the modal.</p>
-    </div>
+  return createPortal(
+    <div className={css.backdrop}>
+      <div className={css.modal}>
+        <h2>Modal Title</h2>
+        <p>This is some content inside the modal.</p>
+      </div>
+    </div>,
+    document.body
   );
 }
